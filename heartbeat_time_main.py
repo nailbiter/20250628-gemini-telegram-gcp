0ceed1f3fa97c7a8b125b7f9afe04e8920bde451
Oleksii Leontiev <alozz1991@gmail.com>
2025-10-04 15:55:00 -0400

    black

--- heartbeat_time_main.py
+++ heartbeat_time_main.py
@@ -7,6 +7,7 @@ from heartbeat import HeartbeatJob
 logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
 
+
 @app.route("/", methods=["POST"])
 def index():
     # The trigger from Pub/Sub via Eventarc is a POST request
@@ -18,6 +19,7 @@ def index():
     except Exception as e:
         logging.error(f"Job failed: {e}")
         return "Job execution failed", 500
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
