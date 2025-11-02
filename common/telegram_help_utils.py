0796c4cb27ef77db457cdc7064b6299dbe42e5e3
Oleksii Leontiev <alozz1991@gmail.com>
2025-11-02 09:53:00 -0500

    stash

---
+++ common/telegram_help_utils.py
@@ -0,0 +1,56 @@
+"""===============================================================================
+
+        FILE: /Users/nailbiter/Documents/forgithub/20250628-gemini-telegram-gcp/common/telegram_help_utils.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2025-11-02T09:29:47.662258
+    REVISION: ---
+
+==============================================================================="""
+
+import os
+import logging
+import typing
+
+import telegram
+from alex_leontiev_toolbox_python.utils.logging_helpers import get_configured_logger
+
+
+class TelegramBotWrapper:
+    def __init__(self, token_envvar: str):
+        TELEGRAM_TOKEN = os.environ.get(token_envvar)
+        self.bot = telegram.Bot(token=TELEGRAM_TOKEN) if TELEGRAM_TOKEN else None
+        self._chat_id: typing.Optional[str] = None
+        self._logger = get_configured_logger(
+            self.__class__.__name__,
+            level="INFO",
+            log_format="%(asctime)s - %(levelname)s - %(message)s",
+        )
+
+    @property
+    def is_able_to_work(self):
+        return self.bot is not None
+
+    @property
+    def chat_id(self) -> typing.Optional[int]:
+        return self._chat_id
+
+    @chat_id.setter
+    def chat_id(self, chat_id: typing.Optional[int]) -> None:
+        self._logger.info(f"set chat_id to {chat_id}")
+        self._chat_id = chat_id
+
+    async def send_message(self, text: str, chat_id: typing.Optional[int] = None):
+        return await self.bot.send_message(
+            text=text, chat_id=self._chat_id if chat_id is None else chat_id
+        )
--- send_telegram_message_callback.py
+++ send_telegram_message_callback.py
@@ -1,12 +1,12 @@
 # actor_server.py
 import os
 import logging
-import telegram
 import asyncio
 from fastapi import FastAPI, Request, Response
 from _actor import add_money, add_note, sleepstart, sleepend
 import functools
 from pymongo import MongoClient
+from common.telegram_help_utils import TelegramBotWrapper
 
 # --- Initialization ---
 logging.basicConfig(
@@ -14,8 +14,10 @@ logging.basicConfig(
 )
 app = FastAPI()
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-bot = telegram.Bot(token=TELEGRAM_TOKEN) if TELEGRAM_TOKEN else None
+# TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+# bot = telegram.Bot(token=TELEGRAM_TOKEN) if TELEGRAM_TOKEN else None
+my_bot = TelegramBotWrapper("TELEGRAM_TOKEN")
+my_bot.chat_id = int(os.environ.get("CHAT_ID"))
 
 MONGO_URL = os.environ.get("MONGO_URL")
 mongo_client = MongoClient(MONGO_URL) if MONGO_URL else None
@@ -35,7 +37,7 @@ async def handle_callback(request: Request):
     Handles a callback_query payload forwarded from the dispatcher
     and echoes the original message text.
     """
-    if not bot:
+    if not my_bot.is_able_to_work:
         logging.error("TELEGRAM_TOKEN not configured.")
         return Response(content="Service not configured", status_code=500)
 
@@ -48,33 +50,32 @@ async def handle_callback(request: Request):
 
     try:
         # Extract the necessary info from the callback_query payload
-        chat_id = payload["message"]["chat"]["id"]
         original_text = payload["message"]["text"]
 
         text = original_text.strip()
-        cmd, *_ = text.split()
-        logging.info(f"cmd: `{cmd}`")
+        logging.info(f"text: {text}")
+        await my_bot.send_message(text)
+        # cmd, *_ = text.split()
+        # logging.info(f"cmd: `{cmd}`")
 
-        is_matched: bool = False
-        for k, cb in COMMANDS.items():
-            if k == cmd:
-                await cb(
-                    text.removeprefix(cmd).strip(),
-                    # send_message=functools.partial(bot.send_message, chat_id=chat_id),
-                    send_message_cb=lambda text: bot.send_message(
-                        text=text, chat_id=chat_id
-                    ),
-                    mongo_client=mongo_client,
-                )
-                is_matched = True
-                break
+        # is_matched: bool = False
+        # for k, cb in COMMANDS.items():
+        #     if k == cmd:
+        #         await cb(
+        #             text.removeprefix(cmd).strip(),
+        #             # send_message=functools.partial(bot.send_message, chat_id=chat_id),
+        #             send_message_cb=my_bot.send_message,
+        #             mongo_client=mongo_client,
+        #         )
+        #         is_matched = True
+        #         break
 
-        if not is_matched:
-            # Formulate the echo message
-            echo_text = f"Button press received from message: '{original_text}'"
+        # if not is_matched:
+        #     # Formulate the echo message
+        #     echo_text = f"Button press received from message: '{original_text}'"
 
-            # Send the echo message back to the chat
-            await bot.send_message(chat_id=chat_id, text=echo_text)
+        #     # Send the echo message back to the chat
+        #     await bot.send_message(chat_id=chat_id, text=echo_text)
 
     except (KeyError, IndexError) as e:
         logging.error(f"Error processing payload, missing key: {e}")
